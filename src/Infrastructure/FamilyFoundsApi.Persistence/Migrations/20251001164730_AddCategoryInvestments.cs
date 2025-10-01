93a1c92976519fe6b839ea70efd0e4d84f9fea45
PhilThson <phil.nevermind@gmail.com>
2025-10-01 18:49:06 +0200

    Migrations

---
+++ src/Infrastructure/FamilyFoundsApi.Persistence/Migrations/20251001164730_AddCategoryInvestments.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyFoundsApi.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryInvestments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { (short)11, "Inwestycje" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)11);
+        }
+    }
+}
